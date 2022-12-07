6257f9c53172300c3e689e41052034b1b0839cc3
HansakaAnuhas <119840634+HansakaAnuhas@users.noreply.github.com>
2022-12-07 13:22:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/SECCOMPLEX/applelkbot.git /hbolkbot
+  git clone https://github.com/SECCOMPLEX/applelkbot.git /applelkbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /applelkbot
