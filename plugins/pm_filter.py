31bd27ff9ef89589b8eb4b18e7d5525f30db81f4
HansakaAnuhas <119840634+HansakaAnuhas@users.noreply.github.com>
2022-12-08 18:49:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -93,6 +93,11 @@ async def next_page(bot, query):
             ]
             for file in files
         ]
+        btn.insert(0,
+                   [InlineKeyboardButton(f'ɪɴꜰᴏ', 'reqinfo'),
+                    InlineKeyboardButton(f'ᴍᴏᴠɪᴇ', 'minfo'),
+                    InlineKeyboardButton(f'ꜱᴇʀɪᴇꜱ', 'sinfo')]
+                  )
         
     if 0 < offset <= 10:
         off_set = 0
@@ -682,6 +687,11 @@ async def auto_filter(client, msg, spoll=False):
             ]
             for file in files
         ]
+        btn.insert(0,
+                   [InlineKeyboardButton(f'ɪɴꜰᴏ', 'reqinfo'),
+                    InlineKeyboardButton(f'ᴍᴏᴠɪᴇ', 'minfo'),
+                    InlineKeyboardButton(f'ꜱᴇʀɪᴇꜱ', 'sinfo')]
+                  )
                 
     if offset != "":
         key = f"{message.chat.id}-{message.id}"
