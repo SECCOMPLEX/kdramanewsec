b26d18e32d681d04f17c3fa07080a43d9c49187c
SECCOMPLEX <95067541+SECCOMPLEX@users.noreply.github.com>
2022-12-08 07:18:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -80,6 +80,7 @@ async def next_page(bot, query):
                 ),
             ]
             for file in files
+            ]
       btn.insert(0, 
                 [
                     InlineKeyboardButton(f'ɪɴꜰᴏ', 'reqinfo'),
